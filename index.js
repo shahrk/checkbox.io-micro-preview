77e65f7ead7377254947ae4cbccd250b9677d623
Chris Parnin <chris.parnin@gmail.com>
2020-04-10 13:20:14 -0400

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const marqdown = require('./marqdown');
+
+app.configure(function () {
+    app.use(express.bodyParser());
+});
+
+
+app.post('/preview', function(req,res)
+{
+    console.log(req.body.markdown);
+    var text = marqdown.render( req.body.markdown );
+    res.send( {preview: text} );
+})
+
+app.listen(port, () => console.log(`Microservice listening on http://localhost:${port}`))
